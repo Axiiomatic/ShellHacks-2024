31c7e0c3c2fbe4c16ca9c7bae9a01ad4d656d00c
Petar Dosev <pdosev@gmail.com>
2024-09-28 13:38:39 -0400

    Fonts

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
 # app.py
 import streamlit as st
 
-st.markdown("""
-<style>
-   h1 {
-      font-size: 2em;
-      text-align: center;
-      text-transform: uppercase;
-   }
-
-   p {
-
-}
-</style>
-""", unsafe_allow_html=True)
-with open('style.css') as f:
-    css = f.read()
-    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
-
+with open( "static/style.css" ) as css:
+    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html=True)
 
 st.title("Treasure Keeper")
 st.write("We help first-generation students with financial issues. Make this a paragraph long.")
