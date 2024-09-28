902cd8be1b043dba461f1a43e6a41a123abea4b5
Petar Dosev <pdosev@gmail.com>
2024-09-28 16:47:48 -0400

    Add user information functionality

--- app.py
+++ app.py
@@ -20,4 +20,39 @@ with tutorial:
 
 st.title("Treasure Keeper")
 st.write("We help first-generation students with financial issues. Make this a paragraph long.")
+
 st.button("Stone and Steel")
+
+def login():
+    st.session_state.logged_in = True
+
+if 'logged_in' not in st.session_state.keys():
+    st.button("Log in", on_click=login)
+
+if 'logged_in' in st.session_state.keys() and st.session_state.logged_in:
+    st.text_input("Name: ", key="name")
+    st.text_input("Pronouns: ", key="pronouns")
+    st.selectbox("Ethnicity: ", (
+        "Latin American",
+        "African American",
+        "Middle Eastern",
+        "Pacific Islander",
+        "White",
+        "Prefer Not To Say",
+    ), key="ethnicity")
+    st.selectbox("Working Status: ", ("N/A", "Full-Time Employed", "Part-Time Employed"), key="work_status")
+
+    if "work_status" in st.session_state.keys() and st.session_state.work_status != "N/A":
+        st.number_input("Last Year's Income: ", key="income")
+        st.text_input("Job Title: ", key="job_title")
+
+    st.selectbox(
+        "Residency Status: ",
+        ("International Student", "In-State Resident Student", "Out-of-State Resident Student"),
+        key="resident_status"
+    )
+    st.selectbox(
+        "Student Status: ",
+        ("Full-time Student", "Part-time Student"),
+        key="student_status"
+    )
