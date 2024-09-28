e4f3641f1fe387377cb69f06ee8bb599bf7b1505
Petar Dosev <pdosev@gmail.com>
2024-09-28 11:58:25 -0400

    Add styling

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 # app.py
 import streamlit as st
 
-st.title("Main Page")
-st.write("Pretty page that says what the project does.")
+st.markdown("""
+<style>
+   h1 {
+      font-size: 2em;
+      text-align: center;
+      text-transform: uppercase;
+   }
+
+   p {
+
+}
+</style>
+""", unsafe_allow_html=True)
+with open('style.css') as f:
+    css = f.read()
+    st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
+
+st.title("Treasure Keeper")
+st.write("We help first-generation students with financial issues. Make this a paragraph long.")
