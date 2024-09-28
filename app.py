2c4b1df8106917ecc08928dcd37cd1ff67c7880a
DeletedPC <53378482+maximo-64@users.noreply.github.com>
2024-09-28 16:01:35 -0400

    actually runnable, most icons removed

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ with open( "static/style.css") as css:
 homeIcon, home, budgetIcon, budget, quizIcon, quiz, tutorial = st.columns(7, vertical_alignment="top", gap="small")
 
 home.page_link("app.py", label="Home")
-homeIcon.image("home.png", width="10")
+homeIcon.image("home.png")
 
 budget.page_link("pages/budget-dashboard.py", label="Budgeting")
-budgetIcon.image("coin.png", width="10")
+budgetIcon.image("coin.png")
 
 quiz.page_link("pages/quizzes.py", label="Quiz Me")
-quizIcon.image("showdown.png", width="10")
+quizIcon.image("showdown.png")
 
 tutorial.page_link("pages/tutorial.py", label="Tutorial")
 
--- pages/quizzes.py
+++ pages/quizzes.py
@@ -8,13 +8,13 @@ with open( "static/style.css" ) as css:
 
 home, budget, quiz, tutorial = st.columns(4, vertical_alignment="top", gap="medium")
 with home:
-    st.page_link("app.py", label="Home", icon="home.png")
+    st.page_link("app.py", label="Home")
 
 with budget:
-    st.page_link("pages/budget-dashboard.py", label="Budgeting", icon="coin.png")
+    st.page_link("pages/budget-dashboard.py", label="Budgeting")
 
 with quiz:
-    st.page_link("pages/quizzes.py", label="Quiz Me", icon="showdown.png")
+    st.page_link("pages/quizzes.py", label="Quiz Me")
 
 with tutorial:
     st.page_link("pages/tutorial.py", label="Tutorial")
