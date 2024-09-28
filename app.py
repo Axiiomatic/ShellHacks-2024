2ce4e4110437c0c1413661b2c3e2138c699f4cf2
DeletedPC <53378482+maximo-64@users.noreply.github.com>
2024-09-28 16:45:29 -0400

    added tutorial icon to each page, still trying to place background image.

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import streamlit as st
 with open( "static/style.css") as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html=True)
 
-homeIcon, home, budgetIcon, budget, quizIcon, quiz, tutorial = st.columns(7, vertical_alignment="top", gap="small")
-
+homeIcon, home, budgetIcon, budget, quizIcon, quiz, tutorialIcon, tutorial = st.columns(8, vertical_alignment="top", gap="small")
 
 st.image("budget_bg.png", )
 home.page_link("app.py", label="Home")
@@ -18,10 +17,11 @@ quiz.page_link("pages/quizzes.py", label="Quiz Me")
 quizIcon.image("showdown.png")
 
 tutorial.page_link("pages/tutorial.py", label="Tutorial")
+tutorialIcon.image("tutorial.png")
 
 
 st.title("Treasure Keeper")
 st.write("We help first-generation students with financial issues. Make this a paragraph long.")
 st.html("""<button>Stone and Steel</button>""")
-st.image("budget_bg.png")
+st.image("budget_vault_bg.png")
 
--- pages/quizzes.py
+++ pages/quizzes.py
@@ -6,8 +6,9 @@ import streamlit as st
 with open( "static/style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html=True)
 
-homeIcon, home, budgetIcon, budget, quizIcon, quiz, tutorial = st.columns(7, vertical_alignment="top", gap="small")
+homeIcon, home, budgetIcon, budget, quizIcon, quiz, tutorialIcon, tutorial = st.columns(8, vertical_alignment="top", gap="small")
 
+st.image("budget_bg.png", )
 home.page_link("app.py", label="Home")
 homeIcon.image("home.png")
 
@@ -18,6 +19,7 @@ quiz.page_link("pages/quizzes.py", label="Quiz Me")
 quizIcon.image("showdown.png")
 
 tutorial.page_link("pages/tutorial.py", label="Tutorial")
+tutorialIcon.image("tutorial.png")
 
 st.title("Quiz")
 st.write("Standalone quizzes on financial literacy that test what you learned in the tutorial. they also give points. ")
